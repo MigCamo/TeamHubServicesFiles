436558be4a08fc064f94313cf01f8f925384ebb0
Miguel Camo <camomiguel1@gmail.com>
2024-06-08 11:02:39 -0600

    actualizacion

--- DataAccessObjects/FileDAO.js
+++ DataAccessObjects/FileDAO.js
@@ -1,9 +1,23 @@
 const { document } = require('../Models');
 
 class FileDAO {
+
     static async saveNewFile(file) {
         return await document.create(file);
     }
+
+    static async getAllProjectDocument(idProject) {
+        return await document.findAll({
+          where: {
+            IdProject: idProject,
+          },
+        });
+    }
+          
+    static async deleteDocumentByID(IdDocument) {
+        return await document.destroy({ where: { IdDocument } });
+    }
+    
 }
 
 module.exports = FileDAO;
